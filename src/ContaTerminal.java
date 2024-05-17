b59e4fcbc1cd7eeb8c1ed3ef4cf998b379a7b7eb
felipeDev01 <paulofelipe06p@gmail.com>
2024-05-17 00:00:44 -0300

    Segunda versão

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,8 +1,21 @@
-
+import java.math.BigDecimal;
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá seja bem vindo ao Banco Nacional Java!");
+        System.out.println("Digite seu nome logo abaixo:");
+        String name = scanner.nextLine();
+        System.out.println("Digite o número de sua conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite o número de sua agência:");
+        String agenciaBanco = scanner.nextLine() ;
 
-        System.out.println("Hello Word");
+         double saldoConta = 237.48;
+
+
+
     }
 
 }
