5cf838c04ebc65fdf99aeb6109a9834a40a2cb37
felipeDev01 <paulofelipe06p@gmail.com>
2024-05-17 22:46:52 -0300

    versao-final-projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-        System.out.println("Olá seja bem vindo ao Banco Nacional Java!");
+    double saldoConta = 1000.0;
+    System.out.println("Olá seja bem vindo ao Banco Nacional Java!");
         System.out.println("Digite seu nome logo abaixo:");
         String name = scanner.nextLine();
         System.out.println("Digite o número de sua conta: ");
@@ -11,8 +12,25 @@ public class ContaTerminal {
         scanner.nextLine();
         System.out.println("Digite o número de sua agência:");
         String agenciaBanco = scanner.nextLine() ;
+        System.out.println("O valor disponível para saque é de: " + saldoConta + " Digite o valor que deseja sacar abaixo:");
 
-         double saldoConta = 237.48;
+        double valorSaque = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Olá " + name + ", " + "obrigado por criar uma conta em nosso banco, sua agência é: " + agenciaBanco + ", " + "conta: " + numeroConta + " e seu saldo de " + valorSaque + " á está disponível para saque.");
+
+        System.out.println("[S] Para continuar e [N] Para finalizar");
+        String respostaUser = scanner.nextLine();
+        if (respostaUser.equalsIgnoreCase("S")){
+            double novoSaldo = saldoConta-valorSaque;
+            System.out.println("Saque efetuado! seu novo saldo é: " + novoSaldo );
+        }else if(respostaUser.equalsIgnoreCase("N")) {
+        System.out.println("Programa Finalizado!");
+        }else {
+            System.out.println("Resposta inválida");
+        }
+
+    scanner.close();
+
 
 
 
